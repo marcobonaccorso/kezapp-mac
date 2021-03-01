e5b192e498a4dda23c53a70187ffa7d0d231010f
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-03-01 12:20:30 +0100

    creazione interfaccia
    abbiamo creato l'interfaccia del progetto

--- kezapp-mac-client/src/app/app.component.ts
+++ kezapp-mac-client/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  nickname: string = "";
+  messaggio: string = "";
+  showTable:boolean=true;
+
+  constructor() { }
+
+  registrazione() {
+
+  }
+
+  inviaTutti() {
+
+  }
+
 }
